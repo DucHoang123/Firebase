c86cde9ee827277edfa8ff7e29b114e94a2b2a72
Hoang Van Duc <duc.hoang@nextfunc.com>
2018-08-01 16:57:35 +0700

    t

--- Home2/app/components/Buttons/Styles.js
+++
@@ -1,8 +0,0 @@
-import { Text, View, StyleSheet } from 'react-native'
-
-export default stylestotal = StyleSheet.create({
-    buttonLogin: {
-        marginHorizontal: 10,
-        borderRadius: 22
-    }
-})
--- Home2/index.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './app/components/index';
-
-AppRegistry.registerComponent('Home2', () => App);
